9bd0ee12c634e48e07a3e29ecd3397e11b433430
yitujohana <141446110+yitujohana@users.noreply.github.com>
2026-07-30 12:51:01 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,49 +1,19 @@
-using Microsoft.EntityFrameworkCore;
-using Agerie365.API.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Add services to the container.
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// CORS Policy
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy => policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-});
-
-// 2. DbContext በ SQLite
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
 
-// 3. Database Automatic Creation
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.EnsureCreated();
-}
-
 // Configure the HTTP request pipeline.
-// የ `if` ህግ ስለተነሳ በ Production ሞድም ቢሆን Swagger ይሰራል
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Agerie365 API v1");
-});
-
-// app.UseHttpsRedirection(); // Redirection ተዘግቷል
-
-app.UseCors("AllowAll");
+app.UseSwaggerUI();
 
 app.UseAuthorization();
-
 app.MapControllers();
+
+app.MapGet("/", () => "Agerie365 API is Running Successfully!");
 
 app.Run();
